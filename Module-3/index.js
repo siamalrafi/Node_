ba5ae67e197dc471b84fea2a1629087525d14b8f
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-03-07 15:07:11 +0600

    new

--- Module-3/index.js
+++ Module-3/index.js
@@ -1,1 +1,13 @@
-console.log('Learn express Js');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running Successfully!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
